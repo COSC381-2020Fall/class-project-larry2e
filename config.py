11105d363ef2bbd2a753a5d4da092e28d0f0e9cb
EC2 Default User <ec2-user@ip-172-26-0-63.ec2.internal>
2020-11-09 05:24:25 +0000

    adding the config file

---
+++ config.py
@@ -0,0 +1,6 @@
+api_key = 'AIzaSyDOPKs1u2xKhpZ1fZgCfnjSEiK0VPNl_5g'
+cse_id = 'f1c281381e2e707a8' 
+
+target_topic = 'montessori how to'
+google_search_maximum_page_number = 10
+gmail_password = "kdvpbgtlqttbwzvf"
